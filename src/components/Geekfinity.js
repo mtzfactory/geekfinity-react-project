cb2a6eaa7c5df1ca9852f5a61a027f66129053ca
joanamrc <xompa.hernan@gmail.com>
2017-10-18 13:02:37 +0200

    added background to Geekfinity.js

--- src/components/Geekfinity.js
+++ src/components/Geekfinity.js
@@ -5,18 +5,41 @@ import Quotes from './Quotes/Quotes'
 import Forecast from './Forecast/Forecast'
 import Search from './Search/Search'
 import Time from './Time/Time'
+import backgroundService from '../services/BackgroundService'
 
 
 class Geekfinity extends Component {
 
+    constructor() {
+        super()
+
+        this.state = {
+            image: ''
+        }
+    } 
+
+    componentDidMount() {
+       
+        backgroundService.getBackground()
+            .then(image => {
+                this.setState({ image })
+            })
+            .catch(function(error) {
+                console.error(error)
+            })
+            
+    }
+
+
     render() {
         return (
-            <main className="geekfinity">
+            <main className="geekfinity bg-full appear-hide" style = {{backgroundImage: "url(" + this.state.image + ")"}}>
                 <h1>Geekfinity</h1>
-                <Search foreColor='black'/>
-                <Forecast foreColor='black'/>
-                <Time />
-                <Quotes foreColor='black'/>
+                
+                <Search foreColor='white'/>
+                <Forecast foreColor='white'/>
+                <Time foreColor='white'/>
+                <Quotes foreColor='white'/>
             </main>
         )
     }
--- src/services/BackgroundService.js
+++ src/services/BackgroundService.js
@@ -1,10 +1,32 @@
+import React, { Component } from 'react';
 import ApiService from './ApiService'
-import * as constants from '../constants/Constants';
+import * as constants from '../constants/BackgroundConstants';
 
 class BackgroundService extends ApiService {
-    // esto es un ejemplo de uso
+
+/* logica para random usuario -> logica random algum -> logica random imagen */
+
+	__randomUser() {
+		const user = Math.floor(Math.random() * constants.FLICKR_ALBUMS.length)
+
+		return constants.FLICKR_ALBUMS[user]
+	}
+
+	__randomImage() {
+		const user = this.__randomUser()
+		const album =  user.albums[Math.floor(Math.random() * user.albums.length)].album
+
+		return this.__get(constants.ALBUM_BASE_URL.replace('[PHOTOSETID]', album).replace('[USERID]', user.user))
+	}
+
     getBackground() {
-        return this.__get(constants.BACKGROUND_BASE_URL)
+
+    	return this.__randomImage()
+    		.then(function(images) {
+    			const image = images.photoset.photo[Math.floor(Math.random() * images.photoset.photo.length)]
+
+				return constants.BACKGROUND_BASE_URL.replace('[FARMID]', image.farm).replace('[SERVERID]', image.server).replace('[PHOTOID]', image.id).replace('[SECRET]', image.secret)
+			})
     }
 }
 
