15f54f7e14d86bc64ecf1c5c66fc1a4ff807c594
Ricardo Martinez <ricardo.martinez.monje@gmail.com>
2017-10-18 19:52:58 +0200

    added albums.json and BackgroundService updated

--- src/constants/BackgroundConstants.js
+++ src/constants/BackgroundConstants.js
@@ -1,14 +1,6 @@
 const APIKEY = '1d71ede807e58a12aeb6fcac778cb9f5'
 
-export const FLICKR_ALBUMS = [
-	{
-		"user": "155048535@N07",
-		"albums": [
-			{ "album": "72157683898107016" },
-			{ "album": "72157686165223472" }
-		]
-	},
-]
+export const JSON_ALBUMS = './json/albums.json'
 
 export const ALBUM_BASE_URL = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${APIKEY}&photoset_id=[PHOTOSETID]&user_id=[USERID]&format=json&nojsoncallback=1`
 
--- src/services/BackgroundService.js
+++ src/services/BackgroundService.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
 import ApiService from './ApiService'
 import * as constants from '../constants/BackgroundConstants';
 
 class BackgroundService extends ApiService {
 
-/* logica para random usuario -> logica random algum -> logica random imagen */
+	constructor() {
+		super()
 
-	__randomUser() {
-		const user = Math.floor(Math.random() * constants.FLICKR_ALBUMS.length)
-
-		return constants.FLICKR_ALBUMS[user]
+		this.FLICKR_ALBUMS = []
 	}
 
-	__randomImage() {
-		const user = this.__randomUser()
-		const album =  user.albums[Math.floor(Math.random() * user.albums.length)].album
+	__randomUser = () => {
 
-		return this.__get(constants.ALBUM_BASE_URL.replace('[PHOTOSETID]', album).replace('[USERID]', user.user))
+		if (this.FLICKR_ALBUMS.length === 0) {
+			return this.__get(constants.JSON_ALBUMS)
+				.then((resp) => {
+					this.FLICKR_ALBUMS = resp
+					
+					return this.FLICKR_ALBUMS[Math.floor(Math.random() * this.FLICKR_ALBUMS.length)]
+				})
+		}
+		else {
+			return new Promise((resolve, reject) => {
+				resolve(this.FLICKR_ALBUMS[Math.floor(Math.random() * this.FLICKR_ALBUMS.length)])
+			})
+		}
+	}
+
+	__randomImage = () => {
+
+		return this.__randomUser()
+			.then((user) => {
+				const album =  user.albums[Math.floor(Math.random() * user.albums.length)].album
+
+				return this.__get(constants.ALBUM_BASE_URL.replace('[PHOTOSETID]', album).replace('[USERID]', user.user))
+			})
 	}
 
     getBackground() {
